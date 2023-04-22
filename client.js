25b8e0fb0bcec2903efba06b57c0a3a6e865964d
Name-3R1C <ericfeng94@hotmail.com>
2023-04-21 23:22:06 -0700

    Moved connect function from play to client

---
+++ client.js
@@ -0,0 +1,17 @@
+const net = require("net");
+
+const connect = function() {
+  const conn = net.createConnection({
+    host: "192.168.1.80",
+    port: "50541",
+  });
+
+  conn.on("data", (data) => {
+    console.log("Received message: " + data);
+  });
+
+  conn.setEncoding("utf-8");
+  return conn;
+};
+
+module.exports = connect;
--- play.js
+++ play.js
@@ -1,18 +1,3 @@
-const net = require("net");
-
-const connect = function() {
-  const conn = net.createConnection({
-    host: "192.168.1.80",
-    port: "50541",
-  });
-
-  conn.on("data", (data) => {
-    console.log("Received message: " + data);
-  });
-
-  conn.setEncoding("utf-8");
-  return conn;
-};
-
+const client = require("./client");
 console.log("Connecting... ");
-connect();
+client();
