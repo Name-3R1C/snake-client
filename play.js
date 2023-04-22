230b37d99c4e2a7df810ef14a60e4246d15170c9
Name-3R1C <ericfeng94@hotmail.com>
2023-04-22 15:04:05 -0700

    Implemented setupInput and handleUserInput

--- play.js
+++ play.js
@@ -1,3 +1,24 @@
 const client = require("./client");
 console.log("Connecting... ");
 client();
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf-8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+const handleUserInput = function(key) {
+  if (key === '\u0003') {
+    process.exit();
+  }
+
+  if (key === 'a') {
+    console.log("key handling");
+  }
+};
+
+setupInput();
